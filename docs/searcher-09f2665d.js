e17c70dd477e1ec1bd962ee04ab328f12b7b60bc
Trevor Ashley <trevor.ashley@gmail.com>
2026-09-22 14:33:30 -0400

    Adding class notes.

--- docs/searcher-09f2665d.js
+++ docs/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ad28a52e.js',
+                path_to_root + 'searchindex-115d0a19.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
